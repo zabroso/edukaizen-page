5a852efd978bc32bd42d9039965d67c32e3a302a
Pablo Marambio Cuzmar (Alumno) <pablo.marambio@usm.cl>
2026-01-28 17:46:36 -0300

    fix: reparar dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-const repository_name = import.meta.env.REPOSITORY_NAME;
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 export default defineConfig({
+  site: "https://zabroso.github.io",
+  base: "edukaizen_page",
   integrations: [tailwind(), react()],
-  site: 'https://zabroso.github.io',
-  base: repository_name
 });
