7ad71fd62d9dac8b1efd404f28c5d4a56c835bee
Pablo Marambio Cuzmar (Alumno) <pablo.marambio@usm.cl>
2026-01-28 17:20:32 -0300

    refactor: utilizar svg en vez de con un .astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+const repository_name = import.meta.env.REPOSITORY_NAME;
 
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: 'https://zabroso.github.io',
-  base: "/edukaizen-page"
+  base: repository_name
 });
